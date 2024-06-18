05531dd4f01dba4487a685613e72556b681dc7c7
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-18 22:45:03 +0300

    public gome page

---
+++ client/src/pages/PublicHome/PublicHome.tsx
@@ -0,0 +1,11 @@
+import PublicView from "../../components/PublicView/PublicView";
+
+const PublicHome = () => {
+  return (
+    <div>
+      <PublicView />
+    </div>
+  );
+};
+
+export default PublicHome;
