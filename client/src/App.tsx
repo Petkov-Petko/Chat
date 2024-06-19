50ade10e00af0db7ecf191b3031a474b6c8f6706
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-19 22:17:46 +0300

    added home view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import PublicHome from "./pages/PublicHome/PublicHome";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<PublicHome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
