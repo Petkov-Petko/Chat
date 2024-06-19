30d6a66a3a24771ea9ad4bcc3d21ed7895654320
Petko Petkov <petkozlatilov1234@gmail.com>
2024-06-19 22:16:36 +0300

    Changed the register to check if the email exists. Added cookie token on login Added verifyUser to check the cookies token added logout (clears the token)

--- server/server.js
+++ server/server.js
@@ -5,11 +5,16 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import cookieParser from 'cookie-parser';
 
+
 const salt = 10;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
 app.use(cookieParser());
 
 const db = mysql.createConnection({
@@ -19,43 +24,90 @@ const db = mysql.createConnection({
     database: "chat"
 });
 
-
-app.post("/register", (req, res) => {
-    const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
-    bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json({ Error: "User not logged in" });
+    }
+    jwt.verify(token, "jwtSecret", (err, decoded) => {
         if (err) {
-            return res.json({ Error: "Error in hashing password" });
+            return res.json({ Error: "Invalid token" });
         }
-
-        const values = [
-            req.body.name,
-            req.body.email,
-            hash
-        ]
-        db.query(sql, [values], (err, result) => {
-            if (err) {
-                return res.json({ Error: "Error in inserting the data" });
-            }
-            return res.json({ Status: "Success" });
-        });
+        else {
+            req.user = decoded.name;
+            next();
+        }
+       
     });
+
+}
+
+app.get('/',verifyUser, (req, res) => {
+    return res.json({ Status: "Success", User: req.user });
 })
 
+app.post("/login", (req, res) => {
+    const sql = "SELECT * FROM login WHERE email = ?";
+    db.query(sql, [req.body.email], (err, result) => {
+        if (err) {
+            return res.json({ Error: "Error in fetching the data" });
+        }
+        if (result.length > 0) {
+            bcrypt.compare(req.body.password.toString(), result[0].password, (err, response) => {
+                if (err) {
+                    return res.json({ Error: "Error in comparing the password" });
+                }
+                if (response) {
+                    const name = result[0].name;
+                    const token = jwt.sign({ name }, "jwtSecret", { expiresIn: "1d" });
+                    res.cookie("token", token);
+                    return res.json({ Status: "Success" });
+                } else {
+                    return res.json({ Error: "Invalid Password" });
+                }
 
+            });
+        } else {
+            return res.json({ Error: "User does not exist" });
+        }
+    });
+});
 
+app.post("/register", (req, res) => {
+    const checkSql = "SELECT * FROM login WHERE email = ?";
+    db.query(checkSql, [req.body.email], (err, result) => {
+        if (err) {
+            return res.json({ Error: "Error in checking the user" });
+        }
+        if (result.length > 0) {
+            return res.json({ Error: "User already exists" });
+        } else {
+            const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
+            bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+                if (err) {
+                    return res.json({ Error: "Error in hashing password" });
+                }
 
+                const values = [
+                    req.body.name,
+                    req.body.email,
+                    hash
+                ]
+                db.query(sql, [values], (err, result) => {
+                    if (err) {
+                        return res.json({ Error: "Error in inserting the data" });
+                    }
+                    return res.json({ Status: "Success" });
+                });
+            });
+        }
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-
+app.get('/logout', (req, res) => {
+    res.clearCookie("token");
+    return res.json({ Status: "Success" }); 
+})
 
 
 
